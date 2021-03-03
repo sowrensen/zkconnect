b98ee45df121a92eeadafe94157a055440d34688
Sowren Sen <sowrensen@gmail.com>
2021-03-03 16:14:29 +0600

    rearranged some code and methods, added some new methods

--- connect.py
+++ connect.py
@@ -11,43 +11,50 @@ from zk.exception import ZKError, ZKErrorConnection, ZKNetworkError
 
 
 class ZkConnect:
-
+    
     def __init__(self, host, port, endpoint, transmission=True):
         """
         Connect to a ZK Teco device and monitor real-time data.
+        
         :param host: The public/private IP address of ZK Teco device
         :param port: The port of ZK Teco device, usually 4370
         :param endpoint: The API endpoint of an external service, where the client will transmit real-time data
         :param transmission: Toggle the transmission of real-time data, True by default
         """
-        self.endpoint = endpoint
         try:
-            self.connection = self._connect(host, port)
+            self.host = host
+            self.port = port
+            self.endpoint = endpoint
             self.transmission = transmission
-            logging.info(
-                'connection established: host: {}, port: {}'.format(host, port))
+            self.connection = None
+            self._connect()
         except (ZKNetworkError, ZKErrorConnection, ZKError) as error:
             logging.error(error)
         except Exception as error:
             logging.error(error)
-
-    def _connect(self, host, port):
+    
+    def _connect(self, reconnect=False):
         """
         Attempt to establish a connection to a ZK Teco device.
-        :param host: The public/private IP address of ZK Teco device
-        :param port: The port of ZK Teco device, usually 4370
-        :return: ZK
+        
+        :param reconnect: Whether it is an attempt to reconnect
         """
-        zk = ZK(ip=host, port=port, force_udp=True, verbose=True)
-        return zk.connect()
-
+        zk = ZK(ip=self.host, port=self.port, verbose=True)
+        self.connection = zk.connect()
+        if reconnect:
+            logging.debug('initiating reconnection...')
+        logging.info(
+            'connection established: host: {}, port: {}'.format(self.host, self.port))
+    
     def _transmit(self, data):
         """
         Transmit real-time data to the specified endpoint.
+        
         :param data: The HTTP payload
         :return: requests.models.Response
         """
         try:
+            logging.debug('Initiating transmission for ' + str(data))
             response = requests.post(self.endpoint, data)
             response.raise_for_status()
             jsonResponse = response.json()
@@ -60,25 +67,36 @@ class ZkConnect:
         finally:
             if not self.connection.is_enabled:
                 self.connection.enable_device()
-
+    
+    def _healthcheck(self):
+        """
+        Check connection health, in case of failure attempt re-establishment.
+        """
+        if not self.connection.is_connect or self.connection is None:
+            self._connect(reconnect=True)
+        else:
+            # This should ensure that the device is still connected
+            logging.debug('Getting device time: {}'.format(self.connection.get_time()))
+    
     def monitor(self):
         """
         Start monitoring and transmitting real-time data.
         """
         if not self.connection:
             raise ZKErrorConnection('Connection is not established!')
-
-        for log in self.connection.live_capture(new_timeout=5):
+        
+        for log in self.connection.live_capture():
             if log is None:
-                pass
+                self._healthcheck()
             else:
-                logging.debug('Initiating transmission for ' + str(log))
                 if self.transmission:
                     self._transmit({
                         'device_user_id': log.user_id,
                         'timestamp': log.timestamp
                     })
-
+                else:
+                    print('Received data: {}'.format(log))
+    
     def disconnect(self):
         """
         Disconnect from the connected ZK Teco device.
@@ -87,32 +105,34 @@ class ZkConnect:
 
 
 class ParseConfig:
-
+    
     @classmethod
     def _validate(cls, config):
         """
         Validate the dictionary structure of config.
+        
         :param config: The dictionary containing device config and endpoint
         """
         if not all(key in config.keys() for key in ['device', 'endpoint']):
             raise Exception('device or endpoint key is missing!')
-
+        
         device = config.get('device')
         if not all(key in device.keys() for key in ['host', 'port']):
             raise Exception('host or port key is missing in device!')
         if None in device.values():
             raise Exception('host or port key is empty in device!')
-
+        
         if config.get('endpoint') is None:
             raise Exception('endpoint cannot be empty!')
-
+        
         if 'transmission' in config.keys() and type(config.get('transmission')) is not bool:
             raise Exception('transmission key must have to be either true or false!')
-
+    
     @classmethod
     def parse(cls, stream):
         """
         Parse a yaml file to a dictionary.
+        
         :param stream: The stream of data
         :return: dict
         """
